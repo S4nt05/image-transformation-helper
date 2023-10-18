784e7969b9d9eac6e5f58abe4d4bcdcf5ee56e18
S4nt05 <60900178+S4nt05@users.noreply.github.com>
2023-10-18 12:14:59 -0600

    Update Anuncio.jsx
    anuncio

--- src/components/Anuncio.jsx
+++ src/components/Anuncio.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 function Anuncio() {
   return (
     <div>
-      {<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8591176787868089"
-     crossorigin="anonymous"></script>}
+     <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8591176787868089"
+     crossorigin="anonymous"></script>
     </div>
   );
 }
