d2bf7bf9dab88cd9a5f326375b974d975fd0efde
S4nt05 <60900178+S4nt05@users.noreply.github.com>
2023-10-18 12:17:19 -0600

    Update Anuncio.jsx
    anuncio

--- src/components/Anuncio.jsx
+++ src/components/Anuncio.jsx
@@ -3,8 +3,7 @@ import React from 'react';
 function Anuncio() {
   return (
     <div>
-     <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8591176787868089"
-     crossorigin="anonymous"></script>
+     <meta name="google-adsense-account" content="ca-pub-8591176787868089">
     </div>
   );
 }
